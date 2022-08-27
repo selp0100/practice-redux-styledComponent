fc7d11801df7f9949f0c74c5d901bd997014eb39
selp0100 <yonng0000@naver.com>
2022-08-27 15:55:28 +0900

    ADD : Use Redux

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import Content from './Content';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Main = () => {
+  const getNum = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
     <MainPage>
       <Title>HELLO</Title>
+      <Title>redux {getNum}</Title>
+      <BtnBox>
+        <Button
+          onClick={() => {
+            dispatch({ type: 'add' });
+          }}
+        >
+          증가
+        </Button>
+        <Button
+          onClick={() => {
+            dispatch({ type: 'minus' });
+          }}
+        >
+          감소
+        </Button>
+      </BtnBox>
       {NUMBER_DATA.map(number => (
         <Content number={number} key={number.id} />
       ))}
@@ -17,6 +38,28 @@ const MainPage = styled.div`
   ${props => props.theme.variables.absoluteCenter}
   width:100vw;
   padding: 3rem;
+`;
+
+const BtnBox = styled.div`
+  ${props => props.theme.variables.flex('center', 'center', 'center')}
+`;
+
+const Button = styled.button`
+  width: 3rem;
+  height: 2rem;
+  margin: 0 3rem;
+  text-align: center;
+  color: white;
+  background-color: ${p => p.theme.style.deepGrey};
+  &:hover {
+    background-color: ${p => p.theme.style.middleGrey};
+    box-shadow: 0.1rem 0.1rem 0.1rem 0.1rem gray;
+  }
+  &:active {
+    color: white;
+    background-color: ${p => p.theme.style.black};
+    box-shadow: none;
+  }
 `;
 
 const Title = styled.main`
--- src/index.js
+++ src/index.js
@@ -5,11 +5,34 @@ import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 import variables from './styles/variables';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const Number = 1;
+
+function reducer(state = Number, action) {
+  switch (action.type) {
+    case 'add':
+      state++;
+      return state;
+
+    case 'minus':
+      state--;
+      return state;
+
+    default:
+      return state;
+  }
+}
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={{ style: theme, variables }}>
-    <GlobalStyle />
-    <Router />
+    <Provider store={store}>
+      <GlobalStyle />
+      <Router />
+    </Provider>
   </ThemeProvider>
 );
